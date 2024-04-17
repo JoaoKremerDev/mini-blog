4710158b05397eac77bd980a955844261dd8894e
JoaoKremerDev <joaokremer.dev@gmail.com>
2024-04-17 11:15:26 -0300

    feat: avatar has been create

---
+++ src/components/Avatar.jsx
@@ -0,0 +1,8 @@
+import styles from './Avatar.module.css';
+
+export function Avatar({ src, hasBorder = true }) {
+
+    return (
+        <img src={src} className={hasBorder ? styles.avatarWithBorder : styles.avatar} alt="Avatar" />
+    )
+}
